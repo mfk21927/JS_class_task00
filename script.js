e2849d47a011430897e99652a9d15d2ba1509b76
mfk21927 <mfk21927@gmail.com>
2024-02-23 23:43:30 +0500

    task done

---
+++ script.js
@@ -0,0 +1,47 @@
+  // Question 01
+        // let userNum = +prompt("enter Num");
+        // let sum = 0;
+        // for (let i = userNum; i >= 1; i--) {
+        //     sum += i;
+        // }
+        // console.log(sum)
+
+        // Question 02
+        // let userNum1 = +prompt("enter Num");
+        // let userNum2 = +prompt("enter Num");
+        // let sumRange = 0;
+        // for(i = userNum1; i<=userNum2; i++){
+        //     sumRange += i;
+        // }
+        // console.log(sumRange)
+
+        // Question 03
+        // let arr = [15, 2, 5, 3, 25]
+        // let sumOfDigit = 0
+        // let count = 0;
+        // let average;
+        // for (let i = 0; i < arr.length; i++) {
+        //     sumOfDigit = sumOfDigit + arr[i];
+        //     average = sumOfDigit / 5;
+        // }
+        // console.log(average)
+
+        // Question 04
+        // let str1 = prompt("enter name 1");
+        // let str2 = prompt("enter name 1");
+        // if(str1.length = str2.length){
+        //     console.log("same")
+        // }
+        // else{
+        //     console.log("not same")
+        // }
+
+        // Question  05
+        // let str = prompt("Enter Name");
+        // let lastCh = str.charAt(str.length - 1)
+        // let firstCh = str.charAt(0)
+
+        // let midOne = str.substring(1, str.length - 1)
+
+        // let finalAns = lastCh + midOne + firstCh;
+        // console.log(finalAns)
